f4bf8dac75bf7d11e126c65d27b66174172362e1
sohamchavan07 <soham777chavan777@gmail.com>
2026-04-30 15:10:23 +0545

    add sentry gem

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = 'https://2d965ad165863dbd3230ef044ae3b166@o4511303396950016.ingest.us.sentry.io/4511303398195200'
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.dsn = "https://2d965ad165863dbd3230ef044ae3b166@o4511303396950016.ingest.us.sentry.io/4511303398195200"
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
 
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
