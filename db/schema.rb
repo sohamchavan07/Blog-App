5a65f962bd456b1d51d4d557ac89dcffbada74ed
sohamchavan07 <soham777chavan777@gmail.com>
2026-05-13 23:11:57 +0545

    fixing 500 Error

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_12_135543) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "legacy_body"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
