44703acc3efec6ea7f68240a41d4412aa8b58f28
sohamchavan07 <soham777chavan777@gmail.com>
2026-05-22 23:33:07 +0545

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     post "/admin/sign_in", to: "devise/sessions#create"
   end
 
-  resource :profile, only: [:show, :edit, :update]
+  resource :profile, only: [ :show, :edit, :update ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
