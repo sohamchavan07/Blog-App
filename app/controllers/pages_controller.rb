14239c3e93f65d076f5fb2dc72d9d47e8f52c55c
sohamchavan07 <soham777chavan777@gmail.com>
2026-08-23 21:21:24 +0545

    add footer links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
-  layout false, only: [ :home ]
+  layout false, only: :home
 
   def home
+  end
+
+  def about
+  end
+
+  def careers
+  end
+
+  def api
+  end
+
+  def guidelines
+  end
+
+  def help_center
   end
 end
